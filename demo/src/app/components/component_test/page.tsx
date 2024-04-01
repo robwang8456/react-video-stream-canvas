659b21172622f0c345cb644032c54e24d2c21b0c
robwang8456 <rwang8456@gmail.com>
2024-04-01 13:50:12 -0700

    Styling changes

--- demo/src/app/components/component_test/page.tsx
+++ demo/src/app/components/component_test/page.tsx
@@ -4,53 +4,53 @@ import React from "react";
 export default function page() {
   return (
     <>
-      <div className="text-black font-bold py-1 px-4 rounded text-3xl">
+      <div className="text-black font-bold rounded text-3xl">
         Component Demos
       </div>
-      <div className="m-4">
-        <div className="m-4">
+      <ol className="list-decimal m-4">
+        <li className="m-4">
           <Link href="/components/component_test/test_video">
             Video component
           </Link>
-        </div>
-        <div className="m-4">
+        </li>
+        <li className="m-4">
           <Link href="/components/component_test/test_canvas">
             Canvas component
           </Link>
-        </div>
-        <div className="m-4">
+        </li>
+        <li className="m-4">
           <Link href="/components/component_test/test_camera_video">
             CameraVideo component
           </Link>
-        </div>
-        <div className="m-4">
+        </li>
+        <li className="m-4">
           <Link href="/components/component_test/test_camera_canvas">
             CameraCanvas component
           </Link>
-        </div>
-        <div className="m-4">
+        </li>
+        <li className="m-4">
           <Link href="/components/component_test/test_stream_video">
             Video component with stream
           </Link>
-        </div>
-        <div className="m-4">
+        </li>
+        <li className="m-4">
           <Link href="/components/component_test/test_stream_canvas">
             Canvas component with stream
           </Link>
-        </div>
-        <div className="m-4">
+        </li>
+        <li className="m-4">
           <Link href="/components/component_test/test_stream_video_with_camera">
             Video component with camera
           </Link>
-        </div>
-        <div className="m-4">
+        </li>
+        <li className="m-4">
           <Link href="/components/component_test/test_stream_canvas_with_camera">
             Canvas component with camera
           </Link>
-        </div>
-        <div className="m-4 font-bold">
-          <Link href="/">Return to homepage</Link>
-        </div>
+        </li>
+      </ol>
+      <div className="font-bold">
+        <Link href="/">Return to homepage</Link>
       </div>
     </>
   );
--- demo/src/app/components/link_test/page.tsx
+++ demo/src/app/components/link_test/page.tsx
@@ -4,39 +4,53 @@ import React from "react";
 export default function page() {
   return (
     <>
-      <div className="text-black font-bold py-1 px-4 rounded text-3xl">
+      <div className="text-black font-bold rounded text-3xl">
         useLink() Demos
       </div>
-      <div className="m-4">
-        <div className="m-4">
-          <Link href="/components/link_test/camera_to_video">Camera to video</Link>
-        </div>
-        <div className="m-4">
-          <Link href="/components/link_test/video_to_video">Video to video</Link>
-        </div>
+      <ol className="list-decimal m-4">
+        <li className="m-4">
+          <Link href="/components/link_test/camera_to_video">
+            Camera to video
+          </Link>
+        </li>
+        <li className="m-4">
+          <Link href="/components/link_test/video_to_video">
+            Video to video
+          </Link>
+        </li>
 
-        <div className="m-4">
-          <Link href="/components/link_test/camera_to_canvas">Camera to canvas</Link>
-        </div>
-        <div className="m-4">
-          <Link href="/components/link_test/video_to_canvas">Video to canvas</Link>
-        </div>
-        <div className="m-4">
-          <Link href="/components/link_test/draw_on_canvas">Draw on canvas</Link>
-        </div>
+        <li className="m-4">
+          <Link href="/components/link_test/camera_to_canvas">
+            Camera to canvas
+          </Link>
+        </li>
+        <li className="m-4">
+          <Link href="/components/link_test/video_to_canvas">
+            Video to canvas
+          </Link>
+        </li>
+        <li className="m-4">
+          <Link href="/components/link_test/draw_on_canvas">
+            Draw on canvas
+          </Link>
+        </li>
 
-        <div className="m-4">
-          <Link href="/components/link_test/switch_source_video">Switch video source</Link>
-        </div>
-        <div className="m-4">
-          <Link href="/components/link_test/switch_source_canvas">Switch canvas source</Link>
-        </div>
-        <div className="m-4">
+        <li className="m-4">
+          <Link href="/components/link_test/switch_source_video">
+            Switch video source
+          </Link>
+        </li>
+        <li className="m-4">
+          <Link href="/components/link_test/switch_source_canvas">
+            Switch canvas source
+          </Link>
+        </li>
+        <li className="m-4">
           <Link href="/components/link_test/link_chain">Link chain</Link>
-        </div>
-        <div className="m-4 font-bold">
-          <Link href="/">Return to homepage</Link>
-        </div>
+        </li>
+      </ol>
+      <div className="font-bold">
+        <Link href="/">Return to homepage</Link>
       </div>
     </>
   );
